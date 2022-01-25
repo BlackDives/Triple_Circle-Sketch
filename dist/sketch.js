a214e41b1f1365dd585b9b51046ab546545dc941
BlackDives <sirkamronwilson00@gmail.com>
2022-01-24 20:00:24 -0600

    initial commit

---
+++ dist/sketch.js
@@ -0,0 +1,18 @@
+function setup() 
+{
+  createCanvas(500, 500);
+  
+  const color1 = color(217, 102, 200, 100)
+  const color2 = color(81, 191, 71, 100)
+  const color3 = color(73, 67, 140, 100)
+  
+  noStroke()
+ 
+  fill(color1)
+  circle(250, 200, 100)
+  fill(color2)
+  circle(290, 250, 100)
+  fill(color3)
+  circle(210, 250, 100)
+
+}
